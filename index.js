91b9f56eef2c5d4f2c779996415780a75ce717cc
TheNotoriousMac <damani.mccaskie@mycavehill.uwi.edu>
2022-10-02 18:24:34 -0400

    Finished.

--- index.js
+++ index.js
@@ -21,7 +21,13 @@ function createSongList() {
 const songList = document.getElementById("songList");
 songList.appendChild(createSongList());
 
-songList.onclick = function(e) {
+const links = document.querySelectorAll("li");
+for(const link of links){
+    link.addEventListener("click", setSong);
+}
+
+function setSong(e) {
+    document.querySelector("#headphones").classList.remove("pulse");
     const source = document.getElementById("source");
     source.src = "songs/" + e.target.innerText;
 
@@ -29,6 +35,7 @@ songList.onclick = function(e) {
 
     player.load();
     player.play();
+    document.querySelector("#headphones").classList.add("pulse");
 }
 
 function playAudio() {
@@ -45,4 +52,11 @@ const slider = document.getElementById("volumeSlider");
 slider.oninput = function (e) {
     const volume = e.target.value;
     player.volume = volume;
+}
+
+function updateProgress(){
+    if(player.currentTime > 0){
+        const progressBar = document.getElementById("progress");
+        progressBar.value = (player.currentTime / player.duration);
+    }
 }
